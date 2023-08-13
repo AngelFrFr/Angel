f852e2f773c479f9548a86f5944602e334610a55
AngelFrFr <142127234+AngelFrFr@users.noreply.github.com>
2023-08-13 09:01:51 -0700

    Update ScriptyScript.lua

--- ScriptyScript.lua
+++ ScriptyScript.lua
@@ -9,17 +9,15 @@ elseif game.PlaceId == 8810636550 then -- Kill others and flex your swords
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/roux/main/swordfighintg.lua"))()
 
 
-elseif game.PlaceId == 9377954133 then -- Mobile Sword Fights (Clasic)
+ elseif game.PlaceId == 9377953133 then -- Mobile Sword Fights Clasic
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/games/MobSwordClasic.lua"))()
-
+ 
 
 elseif game.PlaceId == 6130206252 then -- Mobile Sword Fights
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/games/MobSword.lua"))()
-
-
-
+ 
 else 
  
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/warning.lua"))()
