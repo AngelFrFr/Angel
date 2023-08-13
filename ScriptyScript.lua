7cac7d7f6fa695991a7c97734cc45f9a1de7922f
AngelFrFr <142127234+AngelFrFr@users.noreply.github.com>
2023-08-12 18:34:18 -0700

    Update ScriptyScript.lua

--- ScriptyScript.lua
+++ ScriptyScript.lua
@@ -21,6 +21,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/
 
 
 else 
+ 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/warning.lua"))()
 
 end
