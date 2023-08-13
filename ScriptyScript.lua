a2329399de23b8329b319dcdbc62ee5493a4f41e
AngelFrFr <142127234+AngelFrFr@users.noreply.github.com>
2023-08-12 18:18:46 -0700

    Create ScriptyScript.lua

---
+++ ScriptyScript.lua
@@ -0,0 +1,28 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/e/main/haha.lua"))() -- chat tags <3
+
+if game.PlaceId == 8296410597 then -- Sword Fight Arena
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/roux/main/swordfighintg.lua"))()
+ 
+elseif game.PlaceId == 8810636550 then -- Kill others and flex your swords
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/roux/main/swordfighintg.lua"))()
+
+
+elseif game.PlaceId == 9377954133 then -- Mobile Sword Fights (Clasic)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/games/MobSwordClasic.lua"))()
+
+
+elseif game.PlaceId == 6130206252 then -- Mobile Sword Fights
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/games/MobSword.lua"))()
+
+
+end
+
+-- print(game.PlaceId)
+
+
+
+
