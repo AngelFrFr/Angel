eccd4ff21d5195a5e3367c86a3e5737be148b6b7
AngelFrFr <142127234+AngelFrFr@users.noreply.github.com>
2023-08-14 16:27:41 -0700

    Update ScriptyScript.lua

--- ScriptyScript.lua
+++ ScriptyScript.lua
@@ -5,10 +5,14 @@ if game.PlaceId == 8296410597 then -- Sword Fight Arena
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/games/SwordArena.lua"))()
  
 elseif game.PlaceId == 8810636550 then -- Kill others and flex your swords
-
+ 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BigJoeyJoeyFunnies/roux/main/swordfighintg.lua"))()
 
+ 
+elseif game.PlaceId == 8731279652 then -- Kill others and flex your swords
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/games/StealTime1.lua"))()
+ 
  elseif game.PlaceId == 9377953133 then -- Mobile Sword Fights Clasic
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/games/MobSwordClasic.lua"))()
@@ -18,9 +22,7 @@ elseif game.PlaceId == 6130206252 then -- Mobile Sword Fights
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/games/MobSword.lua"))()
  
-else 
  
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelFrFr/Angel/main/warning.lua"))()
 
 end
 
